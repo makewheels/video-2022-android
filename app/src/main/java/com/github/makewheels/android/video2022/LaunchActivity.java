44c6461efd9973d28b1897dc92ea61cd9e6e1fd1
makewheels <33256842+makewheels@users.noreply.github.com>
2022-04-24 22:41:21 +0800

    安卓有点麻烦啊

--- app/src/main/java/com/github/makewheels/android/video2022/LaunchActivity.java
+++ app/src/main/java/com/github/makewheels/android/video2022/LaunchActivity.java
@@ -1,8 +1,12 @@
 package com.github.makewheels.android.video2022;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import cn.hutool.http.HttpUtil;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        checkLogin();
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkLogin();
+    }
+
+    /**
+     * 检查登陆状态，如果token校验成功返回true，失败返回false
+     */
+    private boolean checkLogin() {
+        SharedPreferences sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
+        String token = sharedPreferences.getString("token", null);
+//        if (token == null) {
+//            return false;
+//        }
+        new Thread() {
+            @Override
+            public void run() {
+                String s = HttpUtil.get("https://www.baidu.com");
+                Log.e("tag", s);
+            }
+        }.start();
+
+        return true;
+    }
+
 }
