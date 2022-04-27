4ee94df4304a344a71df6ea7eb259becad59e9c5
makewheels <33256842+makewheels@users.noreply.github.com>
2022-04-28 00:42:12 +0800

    file upload

--- app/src/main/java/com/github/makewheels/android/video2022/upload/ProgressActivity.java
+++ app/src/main/java/com/github/makewheels/android/video2022/upload/ProgressActivity.java
@@ -1,10 +1,13 @@
 package com.github.makewheels.android.video2022.upload;
 
+import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -13,19 +16,30 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.sdk.android.oss.ClientException;
+import com.alibaba.sdk.android.oss.OSS;
+import com.alibaba.sdk.android.oss.OSSClient;
+import com.alibaba.sdk.android.oss.ServiceException;
+import com.alibaba.sdk.android.oss.common.auth.OSSCredentialProvider;
+import com.alibaba.sdk.android.oss.common.auth.OSSStsTokenCredentialProvider;
+import com.alibaba.sdk.android.oss.model.CompleteMultipartUploadResult;
+import com.alibaba.sdk.android.oss.model.MultipartUploadRequest;
 import com.github.makewheels.android.video2022.R;
 import com.github.makewheels.android.video2022.utils.HttpUtils;
 import com.github.makewheels.android.video2022.utils.ToastUtil;
 
+import cn.hutool.core.io.FileUtil;
+
 
 public class ProgressActivity extends AppCompatActivity {
-    private TextView tv_videoSize;
+    private TextView tv_fileSize;
     private ProgressBar progressBar;
     private TextView tv_percent;
     private TextView tv_shortUrl;
     private Button btn_copy_shortUrl;
 
     private Uri uri;
+    private OSS oss;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +51,7 @@ public class ProgressActivity extends AppCompatActivity {
     }
 
     private void init() {
-        tv_videoSize = findViewById(R.id.tv_item_name);
+        tv_fileSize = findViewById(R.id.tv_fileSize);
         progressBar = findViewById(R.id.progressBar);
         tv_percent = findViewById(R.id.tv_percent);
         tv_shortUrl = findViewById(R.id.tv_shortUrl);
@@ -54,12 +68,52 @@ public class ProgressActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("Range")
+    public String getFilename(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = getContentResolver().query(uri, null,
+                    null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                    Log.e("tag", result);
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    @SuppressLint("Range")
+    public long getFileSize(Uri uri) {
+        long result = 0;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = getContentResolver().query(uri, null,
+                    null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getLong(cursor.getColumnIndex(OpenableColumns.SIZE));
+                }
+            }
+        }
+        return result;
+    }
+
     private void createVideo() {
         uri = getIntent().getExtras().getParcelable("uri");
+        String filename = getFilename(uri);
+        long size = getFileSize(uri);
+        runOnUiThread(() -> tv_fileSize.setText(FileUtil.readableFileSize(size)));
+
         //创建视频
         JSONObject createRequest = new JSONObject();
         createRequest.put("type", "USER_UPLOAD");
-        createRequest.put("originalFilename", "1.mp4");
+        createRequest.put("originalFilename", filename);
         JSONObject createResponse = HttpUtils.post("/video/create", createRequest.toJSONString());
         JSONObject createResponseData = createResponse.getJSONObject("data");
         String shortUrl = createResponseData.getString("shortUrl");
@@ -75,10 +129,35 @@ public class ProgressActivity extends AppCompatActivity {
 
         //上传文件
         JSONObject credentials = getUploadCredentialsResponse.getJSONObject("data");
+        OSSCredentialProvider credentialProvider = new OSSStsTokenCredentialProvider(
+                credentials.getString("accessKeyId"),
+                credentials.getString("secretKey"),
+                credentials.getString("sessionToken")
+        );
+        oss = new OSSClient(getApplicationContext(), credentials.getString("endpoint"), credentialProvider);
+
+        //开始分片上传
+        MultipartUploadRequest<MultipartUploadRequest<?>> uploadRequest = new MultipartUploadRequest<>(
+                credentials.getString("bucket"), credentials.getString("key"), uri);
+        uploadRequest.setPartSize(1024 * 1024);
+        uploadRequest.setProgressCallback((request, currentSize, totalSize) -> {
+            double percent = currentSize * 1.0 / totalSize * 100;
+            runOnUiThread(() -> {
+                progressBar.setProgress((int) percent);
+                tv_percent.setText(String.format("%.2f", percent) + "%");
+            });
+        });
+
+        try {
+            CompleteMultipartUploadResult result = oss.multipartUpload(uploadRequest);
+        } catch (ClientException | ServiceException e) {
+            e.printStackTrace();
+        }
 
         //通知文件上传完成
         HttpUtils.get("/file/uploadFinish?fileId=" + fileId);
         //通知视频源文件上传完成
         HttpUtils.get("/video/originalFileUploadFinish?videoId=" + videoId);
+        runOnUiThread(() -> ToastUtil.success(this, "Upload finished!"));
     }
 }
